ef3175de164189ea8064f6d68cd752f6f8d30ac0
chesminsky <chesminsky@gmail.com>
2018-11-11 18:27:38 +0300

    find user

--- config/passport.config.js
+++ config/passport.config.js
@@ -6,15 +6,27 @@ const config = nconf.get('google');
 
 const User = require('../models/user.model');
 
-passport .use(new GoogleStrategy({
+passport.serializeUser((user, done) => {
+  done(user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(done);
+});
+
+passport.use(new GoogleStrategy({
   callbackURL: '/auth/google/redirect',
   clientID: config.client_id,
   clientSecret: config.client_secret,
 }, (accessToken, refreshToken, profile, done) => {
-  console.log(profile);
-
-  new User({
-    name: profile.displayName,
-    googleId: profile.id,
-  }).save().then(done);
+  User.findOne({googleId: profile.id}).then((currentUser) => {
+    if (currentUser) {
+      done(currentUser);
+    } else {
+      new User({
+        name: profile.displayName,
+        googleId: profile.id,
+      }).save().then(done);
+    }
+  });
 }));
